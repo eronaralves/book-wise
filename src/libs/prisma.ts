9817907da5dc1b61937a1baba4c9bcb09100bb01
eronaralves <alveseronar@gmail.com>
2024-10-21 12:09:54 -0300

    refactor: prisma client

--- src/libs/prisma.ts
+++ src/libs/prisma.ts
@@ -1,7 +1,12 @@
-import { PrismaClient } from "@prisma/client"
- 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
- 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
- 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
+export { prisma };
