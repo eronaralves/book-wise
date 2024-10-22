0c47999041b63e730855af801a41edcb6cb39724
eronaralves <alveseronar@gmail.com>
2024-10-22 13:36:18 -0300

    refactor: eslint ignore

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com', 'images.unsplash.com', 'images/books', 'avatars.githubusercontent.com'],
   },
+  experimental: {
+    outputStandalone: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
